ec671267bb9f1afc558cb0dcf7ec1d56502a5742
nagiyu <otonagiyu@gmail.com>
2024-12-07 23:23:39 +0900

    Update deployment workflow and configure forwarded headers for authentication

--- GoogleOAuthDemo/Program.cs
+++ GoogleOAuthDemo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -23,6 +24,11 @@ builder.Services.AddAuthentication(options =>
 {
     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 });
 
 builder.Services.AddAuthorization();
@@ -77,6 +83,8 @@ app.Use(async (context, next) =>
 });
 
 app.UseAuthorization(); // 必須: 認可チェック
+
+app.UseForwardedHeaders();
 
 app.MapControllerRoute(
     name: "default",
