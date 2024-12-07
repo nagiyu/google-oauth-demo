a54db2ced5376470ec732e4e13233a1a3eb4ea34
nagiyu <otonagiyu@gmail.com>
2024-12-07 16:52:02 +0900

    Remove UserSecretsId from project file and configure Google authentication with cookie support in Program.cs; update appsettings.json for Google credentials

--- GoogleOAuthDemo/Program.cs
+++ GoogleOAuthDemo/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Google 認証をデフォルトスキームに設定
 builder.Services.AddAuthentication()
+.AddCookie("Cookies")
     .AddGoogle(googleOptions =>
     {
         googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
