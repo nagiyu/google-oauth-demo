f524febdc96c4be833656eb103e969e238ac0d5c
nagiyu <otonagiyu@gmail.com>
2024-12-07 13:38:51 +0900

    Refactor middleware configuration in Program.cs to streamline static file handling and HTTPS redirection

--- GoogleOAuthDemo/Program.cs
+++ GoogleOAuthDemo/Program.cs
@@ -30,10 +30,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
-app.UseStaticFiles();
 
 // ミドルウェアの設定
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
